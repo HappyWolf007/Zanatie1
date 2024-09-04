4ebc7ea883ea669839f774c78d7898a06e23f862
Денис Мамонтов <217019@edu.fa.ru>
2024-09-04 15:20:03 +0300

    Добавьте файлы проекта.

---
+++ Dll1/dllmain.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include <windows.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+BOOL WINAPI DllMain(HINSTANCE hDll, DWORD  dwReason, LPVOID lpReserved) {
+    switch (dwReason) {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+extern "C" __declspec(dllexport) void ProcessFile(string fileName) {
+    ifstream inputFile(fileName);
+    ofstream outputFile("output.txt");
+    string line;
+    int replacements = 0; 
+
+    while (!inputFile.eof() && inputFile.good()) {
+        getline(inputFile, line);
+
+       
+        for (char& ch : line) {
+            if (ch < 48) {
+                ch = ' ';
+                replacements++;
+            }
+        }
+
+        outputFile << line << std::endl;
+        
+    }
+    
+
+    std::cout << "File processed successfully. Check \"output.txt\" for result." << std::endl;
+    std::cout << "Count replacements: " << replacements << std::endl;
+}
---
+++ Zanatie1/Zanatie1.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    HMODULE hDll = LoadLibrary("Dll1.dll");
+    if (!hDll)
+    {
+        cerr << "Failed to load library" << endl;
+        return GetLastError();
+    }
+
+    void (*ProcessFile)(string) = (void (*)(string))GetProcAddress(hDll, "ProcessFile");
+    if (!ProcessFile)
+    {
+        cerr << "Failed to get ProcessFile procedure address from DLL.";
+        return GetLastError();
+    }
+
+    ProcessFile("input.txt");
+
+    return 0;
+}
